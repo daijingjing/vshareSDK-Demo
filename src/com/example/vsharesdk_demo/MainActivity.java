4832a9c1d4f412fa97be9e905dbe0dde56ac148e
Dai Jingjing <jjyyis@qq.com>
2015-03-17 15:57:54 +0800

    增加用户第三方接口认证的测试代码、

--- src/com/example/vsharesdk_demo/MainActivity.java
+++ src/com/example/vsharesdk_demo/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.vsharesdk_demo;
 
 import com.bumptech.glide.Glide;
+import com.temobi.sx.sdk.vshare.SDKInit;
 import com.temobi.sx.sdk.vshare.player.VideoPlayer;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -20,11 +23,21 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private static final int ACTIVITY_CODE_TO_RECORDER = 0x1234;
+	private static final int source_id = 1;
+
+	ProgressDialog loginDlg = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				doLogin();
+			}
+		});
 
 		findViewById(R.id.recorder).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -33,12 +46,40 @@ public class MainActivity extends Activity {
 				double locLat = 0d;
 				
 				Intent intent = new Intent(MainActivity.this, com.temobi.sx.sdk.vshare.recorder.RecorderActivity.class);
-				intent.putExtra("mobile", "13903510000");
 				intent.putExtra("locAddr", "山西省太原市");
 				intent.putExtra("locLon", locLon); // double 类型
 				intent.putExtra("locLat", locLat); // double 类型
 				
 				startActivityForResult(intent, ACTIVITY_CODE_TO_RECORDER);
+			}
+		});
+	}
+	
+	private synchronized void doLogin() {
+		
+		findViewById(R.id.login).setEnabled(false);
+		
+		EditText edt_mobile = (EditText)findViewById(R.id.login_mobile);
+		EditText edt_code = (EditText)findViewById(R.id.login_code);
+		
+		loginDlg = new ProgressDialog(this, ProgressDialog.THEME_HOLO_LIGHT);
+		loginDlg.setMessage("正在启动...");
+		loginDlg.show();
+		
+		SDKInit.login(getApplication(), edt_mobile.getText().toString(), edt_code.getText().toString(), source_id, new SDKInit.SDKInitCallback() {
+			
+			@Override
+			public void onLoginSucceed(String userId) {
+				findViewById(R.id.login).setVisibility(View.GONE);
+				findViewById(R.id.work).setVisibility(View.VISIBLE);
+				loginDlg.dismiss();
+			}
+			
+			@Override
+			public void onLoginError() {
+				findViewById(R.id.login).setEnabled(true);
+				loginDlg.dismiss();
+				Toast.makeText(MainActivity.this, "启动失败，请检查网络设置后重试", Toast.LENGTH_LONG).show();
 			}
 		});
 	}
